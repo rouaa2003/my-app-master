0c0ceb20200a88ef8e4c74d68488e08b4242336a
khaled muwahed <khaled.muwahed@engie.com>
2024-08-02 17:14:49 +0100

    feat: fix header and filters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <header id="header" >
-      <h1 className='h'>Welcome to the Blue Market</h1>
-      <p className='h-p'>Buy and sell used products easily</p>
+    <header id="header">
+      <h1 className="h">Welcome to the Blue Market</h1>
+      <p className="h-p">Buy and sell used products easily</p>
     </header>
   );
 }
 
 export default Header;
-
