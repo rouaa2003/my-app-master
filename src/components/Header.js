f383fc84a8cc37de47cb95ef5981b49166e1c616
khaled muwahed <khaled.muwahed@engie.com>
2024-08-04 18:57:38 +0100

    feat update styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <header id="header">
-      <h1 className="h">Welcome to the Blue Market</h1>
-      <p className="h-p">Buy and sell used products easily</p>
+      <text className="h">Welcome to the Blue Market</text>
+      <text className="h-p">Buy and sell used products easily</text>
     </header>
   );
 }
